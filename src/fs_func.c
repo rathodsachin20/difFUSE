1b19c098d481fe6f9a9e6559585c85bb62bc6133
Sahaj Biyani <sahaj@Sahajs-MacBook-Pro.local>
2014-10-31 01:11:07 -0700

    func

--- src/fs_func.c
+++ src/fs_func.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include <fuse.h>
 
-int fs_getattr(const char *filename, struct stat *st){
-    struct node* inode = get_inode(filename);
+int fs_getattr(const char *filepath, struct stat *st){
+    struct node* inode = get_inode(filepath);
     //if fails then return error
     st->st_dev = inode->;//Id of device containing file
     st->st_ino = inode->inumber;
@@ -14,6 +15,21 @@ int fs_getattr(const char *filename, struct stat *st){
     st->st_size = inode->file_size;
     st->st_atime = inode->accessed;
     st->st_mtime = inode->file_modified;
+
+    return 0;
+}
+
+int fs_mknod(const char *filepath, mode_t mode, dev_t dev){
+    if(path is empty string)
+	return ENOENT
+    if(file name already exists)
+	return EEXIST
+    if(component of path denied search/write permission)
+	return EACCES
+
+    //fetch and assign inode to this new file
+    //set permissions
+    //assign group id of file to that of parent group id
 
     return 0;
 }
