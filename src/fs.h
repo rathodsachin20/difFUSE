2a85f0e949d47500c3524c2012e5788ad086de4b
Sachin Rathod <rathodsachin20@gmail.com>
2014-11-04 15:37:07 -0800

    Added fs_write function.

---
+++ src/fs.h
@@ -0,0 +1,19 @@
+#ifndef __fs_h__
+#define __fs_h__
+
+#include "fsutil.c"
+
+
+int fs_open();
+
+int fs_close()
+
+int fs_create(const char* filepath, perm_t perms);
+
+int fs_write(const char* filepath, long offset, const char* buffer, long size, FILE* fp);
+
+int fs_read(const char* filepath, long offset, char* buffer, long size, FILE* fp);
+
+long get_file_block_num(long offset, struct inode node, int block_pos);
+
+#endif
