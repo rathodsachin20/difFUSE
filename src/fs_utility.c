f6766fe30bedb76859bf00f9b8224eaafc6511f3
Sahaj Biyani <sahaj@169-231-109-52.wireless.ucsb.edu>
2014-11-04 14:24:41 -0800

    namei

--- src/fs_utility.c
+++ src/fs_utility.c
@@ -13,13 +13,13 @@ long int fs_namei(FILE* fp, const char* filep){
 	get_inode_struct(fp, 1, &working_inode);
 	//long int block_num = working_inode->direct_blocks[0];
 	//get_block(fp, block_num);
-	struct directory* dr;
-	long int inode_num;
+	struct directory dr;
+	long int inode_num=0;
 	fread(&dr, sizeof(struct directory), 1, fp);
 	int i=0;
 	while(i<MAX_NUM_FILE){
-	    if( strcmp(fname,dr->name[i])==0 ){
-		inode_num = dr->inode_num[i];
+	    if( strcmp(fname,dr.name[i])==0 ){
+		inode_num = dr.inode_num[i];
 		break;
 	    }
 	}
