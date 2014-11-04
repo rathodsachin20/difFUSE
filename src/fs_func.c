a109306e83eb85f68377179c080563bf6c98c0b1
Sahaj Biyani <sahaj@169-231-109-52.wireless.ucsb.edu>
2014-11-04 15:19:21 -0800

    read

--- src/fs_func.c
+++ src/fs_func.c
@@ -48,28 +48,43 @@ int fs_mknod(const char *filepath, mode_t mode, dev_t dev){
 	return EEXIST
     if(component of path denied search/write permission)
 	return EACCES
-
     //fetch and assign inode to this new file
     //set permissions
     //assign group id of file to that of parent group id
-
     return 0;
 }
 
 int fs_open(const char* filepath, struct fuse_file_info* ffi, FILE* fp){
     //check for errors
     //if file does not exist then create a new file
-    //get inode
+    //use namei to get inode
     //get the data block using inode
-    
+    //write back the data block and inode when done
+    long int inode_num = fs_namei(fp, filepath);
+    if(inode_num == 0){
+	printf("file doesnt exist, creating...");
+	fs_create(filepath, 0, ffi, fp);
+    else{
+    printf("do nothing");
+    }
 
     return fd; //return file descriptor
 }
 
 int fs_read(const char *filepath, char *buf, size_t count, off_t offset, struct fuse_file_info * ffi, FILE* fp){
-//check if user has permission to read
-struct inode* inode = get_inode(filepath)
-if(inode->file_size < offset)
-    return error;
-//get data from block and display
+    //check if user has permission to read
+    if(inode->file_size < offset)
+	return error;
+    long int inode_num = 0, block_num=0;
+    inode_num = fs_namei(fp, filepath);
+    if(inode_num == 0)
+	printf("file does not exist");
+    struct inode nodep;
+    get_inode_struct(fp, inode_num, &nodep);
+    block_num = nodep.direct_blocks[0];
+    fseek(fp, BLOCK_SIZE * block_num + offset, SEEK_SET);
+    fread(buf, count, 1, fp);
+    //get data from block and display
 }
+
+
